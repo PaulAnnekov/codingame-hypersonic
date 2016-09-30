d7a4b00c481df9c44e13aee3f240b9ff4501e1b5
Paul Annekov <paul.annekov@gmail.com>
2016-09-30 11:15:38 +0300

    formatting

--- hypersonic.dart
+++ hypersonic.dart
@@ -81,9 +81,9 @@ class Game {
             Point pos = new Point(int.parse(inputs[2]), int.parse(inputs[3]));
             int param1 = int.parse(inputs[4]);
             int param2 = int.parse(inputs[5]);
-            if (entityType==0) {
+            if (entityType == 0) {
                 players[owner] = {'pos': pos, 'bombs': param1, 'range': param2};
-            } else if (entityType==1) {
+            } else if (entityType == 1) {
                 bombsWatcher.addBomb(pos, {'owner': owner, 'countdown': param1,
                     'range': param2});
             }
@@ -91,9 +91,14 @@ class Game {
     }
 
     void _checkSettle() {
-        if (target != null && myLocation==target && players[myId]['bombs']>0 && targetType['box']) {
+        if (target != null && myLocation == target &&
+            players[myId]['bombs'] > 0 && targetType['box']) {
             Logger.info('settled a BOMB');
-            bombsWatcher.addBomb(target, {'owner': myId, 'countdown': countdown, 'range': players[myId]['range']});
+            bombsWatcher.addBomb(target, {
+                'owner': myId,
+                'countdown': countdown,
+                'range': players[myId]['range']
+            });
             nextAction = 'BOMB';
             target = null;
         }
@@ -103,14 +108,15 @@ class Game {
         var aStar = new AStar(map, bombsWatcher);
         var affectedBoxes = bombsWatcher.getAffectedBoxes();
         // if no target or target already destroyed or to be destroyed - find new target
-        if (target == null || (!targetType['box'] || affectedBoxes.contains(targetPos))) {
+        if (target == null ||
+            (!targetType['box'] || affectedBoxes.contains(targetPos))) {
             Logger.debug('searching');
             var spiralProcessor = new SpiralProcessor(map, myLocation);
-            var box, boxes = {};
+            var box,
+                boxes = {};
             while ((box = spiralProcessor.getNext()) != null) {
                 Logger.debug('box near ${box}');
-                if (affectedBoxes.contains(box))
-                {
+                if (affectedBoxes.contains(box)) {
                     Logger.info("it's marked as 'to destroy'");
                     continue;
                 }
@@ -138,18 +144,19 @@ class Game {
                 }*/
                 target = box['path'][1];
                 targetPos = box['path'][0];
-                nextStep = box['path'][box['path'].length-2];
+                nextStep = box['path'][box['path'].length - 2];
                 Logger.info('target ${target}');
                 Logger.info('toDestroy ${targetPos}');
                 Logger.info('nextStep ${nextStep}');
             } else {
-              target = null;
+                target = null;
             }
         } else {
             var path = aStar.path(myLocation, target);
             Logger.info('next path ${path}');
             // we can stay near target and wait till free bombs
-            nextStep = path.length > 1 ? path[path.length-2] : path[path.length-1];
+            nextStep =
+            path.length > 1 ? path[path.length - 2] : path[path.length - 1];
         }
     }
 
@@ -183,7 +190,8 @@ class Game {
             List<List<int>> directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
             var choices = [];
             directions.forEach((direction) {
-                var choice = new Point(myLocation.x+direction[0], myLocation.y+direction[1]);
+                var choice = new Point(
+                    myLocation.x + direction[0], myLocation.y + direction[1]);
                 if (map.isOutOfMap(choice.x, choice.y))
                     return;
                 var type = map.cellType(choice);
@@ -195,7 +203,7 @@ class Game {
             if (choices.isEmpty) {
                 Logger.info('you are doomed');
             } else {
-              nextStep = choices[0];
+                nextStep = choices[0];
             }
         } else {
             nextStep = myLocation;
@@ -217,7 +225,7 @@ class Game {
         Logger.info('loop');
         map.updateFromInput();
         _readEntities();
-        myLocation = players[myId]['pos']/*new Point(5,0)*/;
+        myLocation = players[myId]['pos'] /*new Point(5,0)*/;
         Logger.debug('before algo');
         targetType = targetPos != null ? map.cellType(targetPos) : null;
         nextAction = null;
@@ -225,94 +233,101 @@ class Game {
         _checkTarget();
         _checkEnemy();
         _checkOnFire();
-        print((nextAction != null ? nextAction : 'MOVE')+' ${nextStep.x} ${nextStep.y}');
+        print((nextAction != null ? nextAction : 'MOVE') +
+            ' ${nextStep.x} ${nextStep.y}');
         Logger.debug('end');
     }
 }
 
 class BombsWatcher {
-  Map<Point, Map> _bombs = {};
-  static final List<List<int>> directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-  GameMap _map;
+    Map<Point, Map> _bombs = {};
+    static final List<List<int>> directions = [[1, 0], [0, 1], [-1, 0], [0, -1]
+    ];
+    GameMap _map;
 
-  BombsWatcher(this._map);
+    BombsWatcher(this._map);
 
-  void addBomb(Point pos, Map info) {
-    _bombs[pos] = info;
-  }
+    void addBomb(Point pos, Map info) {
+        _bombs[pos] = info;
+    }
 
-  List<Point> _getAffectedBoxes(Point pos, Map info) {
-    List<Point> boxes = [];
-    directions.forEach((direction) {
-      for (var i = 1; i < info['range']; i++) {
-        var cell = new Point(pos.x+direction[0]*i, pos.y+direction[1]*i);
-        if (_map.isOutOfMap(cell.x, cell.y))
-          break;
-        var type = _map.cellType(cell);
-        // wall blocks fire
-        if (type['wall'])
-          break;
-        if (type['box']) {
-            Logger.debug('to destroy cell ${cell} from ${pos} by dir ${direction}');
-          boxes.add(cell);
-          break;
-        }
-      }
-    });
+    List<Point> _getAffectedBoxes(Point pos, Map info) {
+        List<Point> boxes = [];
+        directions.forEach((direction) {
+            for (var i = 1; i < info['range']; i++) {
+                var cell = new Point(
+                    pos.x + direction[0] * i, pos.y + direction[1] * i);
+                if (_map.isOutOfMap(cell.x, cell.y))
+                    break;
+                var type = _map.cellType(cell);
+                // wall blocks fire
+                if (type['wall'])
+                    break;
+                if (type['box']) {
+                    Logger.debug(
+                        'to destroy cell ${cell} from ${pos} by dir ${direction}');
+                    boxes.add(cell);
+                    break;
+                }
+            }
+        });
 
-    return boxes;
-  }
+        return boxes;
+    }
 
-  bool isBomb(Point pos) {
-    return _bombs.containsKey(pos);
-  }
+    bool isBomb(Point pos) {
+        return _bombs.containsKey(pos);
+    }
 
-  /**
-   * Is point will be destroyed on next step.
-   */
-  List<Point> isOnFire(Point point) {
-      List<Point> sides = [];
-      for (var pos in _bombs.keys) {
-          var info = _bombs[pos];
-          if (info['countdown']>2)
-              continue;
-          var isKill = false;
-          for (var direction in directions) {
-              for (var i = 0; i < info['range']; i++) {
-                  var cell = new Point(pos.x+direction[0]*i, pos.y+direction[1]*i);
-                  if (_map.isOutOfMap(cell.x, cell.y))
-                      break;
-                  var type = _map.cellType(cell);
-                  // wall blocks fire
-                  if (type['wall'])
-                      break;
-                  if (cell == point) {
-                      Logger.debug('bomb ${pos} will kill you at ${cell}');
-                      sides.add(new Point(cell.x-direction[0], cell.y-direction[1]));
-                      // fire can strike us from both sides
-                      if (info['range'] > i + 1) {
-                          sides.add(new Point(cell.x+direction[0], cell.y+direction[1]));
-                      }
-                      isKill = true;
-                      break;
-                  }
-              }
-              if (isKill)
-                  break;
-          };
-          if (sides.length>3)
-              break;
-      };
-      return sides;
-  }
+    /**
+     * Is point will be destroyed on next step.
+     */
+    List<Point> isOnFire(Point point) {
+        List<Point> sides = [];
+        for (var pos in _bombs.keys) {
+            var info = _bombs[pos];
+            if (info['countdown'] > 2)
+                continue;
+            var isKill = false;
+            for (var direction in directions) {
+                for (var i = 0; i < info['range']; i++) {
+                    var cell = new Point(
+                        pos.x + direction[0] * i, pos.y + direction[1] * i);
+                    if (_map.isOutOfMap(cell.x, cell.y))
+                        break;
+                    var type = _map.cellType(cell);
+                    // wall blocks fire
+                    if (type['wall'])
+                        break;
+                    if (cell == point) {
+                        Logger.debug('bomb ${pos} will kill you at ${cell}');
+                        sides.add(new Point(
+                            cell.x - direction[0], cell.y - direction[1]));
+                        // fire can strike us from both sides
+                        if (info['range'] > i + 1) {
+                            sides.add(new Point(
+                                cell.x + direction[0], cell.y + direction[1]));
+                        }
+                        isKill = true;
+                        break;
+                    }
+                }
+                if (isKill)
+                    break;
+            };
+            if (sides.length > 3)
+                break;
+        };
+        return sides;
+    }
 
-  List<Point> getAffectedBoxes() {
-    List<Point> boxes = [];
-    _bombs.forEach((pos, info) {
-      boxes.addAll(_getAffectedBoxes(pos, info));
-    });
-    return boxes;
-  }
+    List<Point> getAffectedBoxes() {
+        List<Point> boxes = [];
+        _bombs.forEach((pos, info) {
+            boxes.addAll(_getAffectedBoxes(pos, info));
+        });
+        return boxes;
+    }
 }
 
 /*List<int> getPositionBetween(List<int> point1, List<int> point2) {
@@ -350,11 +365,11 @@ class SpiralProcessor {
     Point getNext() {
         bool isFound = false;
         while (!_isEnd) {
-            var _angleFactor = PI/2/(radius*2);
+            var _angleFactor = PI / 2 / (radius * 2);
             _cellsPerLoop[radius] ??= 0;
-            while (_angle < 2*PI) {
-                var x = _point.x+_nearest(radius*cos(_angle)),
-                    y = _point.y+_nearest(radius*sin(_angle));
+            while (_angle < 2 * PI) {
+                var x = _point.x + _nearest(radius * cos(_angle)),
+                    y = _point.y + _nearest(radius * sin(_angle));
                 _angle += _angleFactor;
                 if (_map.isOutOfMap(x, y))
                     continue;
@@ -405,21 +420,22 @@ class AStar {
         var gScore = {from: 0};
         var fScore = {from: from.distanceTo(to)};
         while (!openSet.isEmpty) {
-            current = openSet.reduce((first, second)=>
+            current = openSet.reduce((first, second) =>
             fScore[first] < fScore[second] ? first : second);
             if (current == to)
                 return _getPath(cameFrom, current);
             openSet.remove(current);
             closedSet.add(current);
             for (var i = 0; i < 4; i++) {
-                var x = current.x+neighborX[i];
-                var y = current.y+neighborY[i];
+                var x = current.x + neighborX[i];
+                var y = current.y + neighborY[i];
                 var neighbor = new Point(x, y);
                 if (_map.isOutOfMap(x, y))
                     continue;
                 var type = _map.cellType(neighbor);
                 // boxes are obstacles, but only when it's not target box
-                if (neighbor != to && (type['obstacle'] || bombsWatcher.isBomb(neighbor)))
+                if (neighbor != to &&
+                    (type['obstacle'] || bombsWatcher.isBomb(neighbor)))
                     continue;
                 if (closedSet.contains(neighbor))
                     continue;
@@ -464,4 +480,4 @@ class Logger {
     }
 }
 
-enum LogLevels {DEBUG, INFO, DISABLE}
+enum LogLevels { DEBUG, INFO, DISABLE }
